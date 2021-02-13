3bf1f209510e10bcb10a70f0a1384935e56a01c9
Abdeldjalil-H <djaloulehez3@gmail.com>
2021-02-13 22:33:27 +0100

    Add some quai functions

--- quai.c
+++ quai.c
@@ -52,3 +52,49 @@ Cette fonction vérifie si une pile est remplie ou non.
 int PileRemplie(quai_t q){
     return (q.premierTrain == q.capacite);
 }
+/*
+Rechercher des trains qui ne sont pas dans les quais
+*/
+quai_t TrouverDesTrainDisponible(quai_t q, train_t* Trains, int numTrain){
+    for (int i=0;i<numTrain;i++){
+        if((Trains[i].kmRestant == -1)&&(!PileRemplie(q))){
+            Trains[i].kmRestant=0;//train dans le quai
+            Empiler(Trains[i],&q);
+        }
+    }
+    return q;
+}
+
+int AjouterQuais(quai_t * Quais,int NumQuai,chaine IdentifiantQuai, int capacity, train_t* Trains, int NumTrain){
+    //creer un quai avec un identifiant et une capacité
+    Quais[NumQuai]=CreerUnParkingRail(IdentifiantQuai,capacity);
+    // rechercher des trains disponible pour remplir le quai 
+    Quais[NumQuai] = TrouverDesTrainDisponible(Quais[NumQuai],Trains,NumTrain);
+
+    return NumQuai+1;
+}
+
+void UpdateQuais(quai_t* Quais, int NumQuai,train_t* Trains, int NumTrain){
+    for (int j=0;j<NumTrain;j++){
+        for(int i=0;i<NumQuai;i++)
+            if((Trains[j].kmRestant == -1))
+                if(!PileRemplie(Quais[i])){
+                    Trains[j].kmRestant = 0;
+                    Empiler(Trains[j],&Quais[i]);
+                }
+    }
+}
+
+void AfficherParkingRail(quai_t* ParkingRails, int NumParkingRail){
+
+    printf("Le nombre de Quais dans la gare est de %d\n \n",NumParkingRail);
+    for (int i=0;i<NumParkingRail;i++){
+        printf("Le nombre de train dans le quai %s est de %d \n",ParkingRails[i].id,ParkingRails[i].premierTrain+1);
+        for (int j=ParkingRails[i].premierTrain;j>=0;j--)
+            printf("le train ID=%d prendra le départ en %d position\n",ParkingRails[i].listeDesTrains[j].id,ParkingRails[i].premierTrain-j+1);
+    
+
+
+    }
+
+}
--- quai.h
+++ quai.h
@@ -4,7 +4,7 @@
 
 typedef struct quai{
     int id;
-    listeTrain_t listeTrain;
+    listeTrain_t listeDesTrains;
     int premierTrain; //sommet de la pile
     int capacite;
 }quai_t;
@@ -19,4 +19,11 @@ int PileVide(quai_t *);
 
 int PileRemplie(quai_t);
 
+quai_t TrouverDesTrainDisponible(quai_t, train_t*, int);
+
+int AjouterQuais(quai_t*, int, chaine, int, train_t*, int);
+
+void UpdateQuais(quai_t*, int, train_t*, int);
+
+void AfficherParkingRail(quai_t*, int);
 #endif
